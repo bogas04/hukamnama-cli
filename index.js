d39850fda46457eb158dd190091d6ce8af0c950b
Divjot Singh <bogas04@gmail.com>
2016-06-06 14:34:16 +0530

    general fixes

--- index.js
+++ index.js
@@ -15,20 +15,14 @@ console.log("Waheguru Ji Ka Khalsa Waheguru Ji Ki Fateh!");
 
 const getHukamnama = (hymn = randomAng) => {
   console.log(`Fetching hymn ${hymn}`);
+
   return fetch(`http://api.sikher.com/hymn/${hymn}`)
     .then(r => { console.log(`Downloaded hymn ${hymn}`); return r.json(); })
-    // .then(lines => fetch(`http://api.sikher.com/hymn/${lines.slice(-1)[0].hymn}`))
-    // .then(r => r.json())
-    .then(lines => {
-      //console.log(`Downloaded hymn ${lines[0].hymn}`);
-      return Promise.resolve(lines.reduce((para, line) => para += `\t${line.text}\n\r${line.translation.text}\n\r`, ''));
-    });
+    .then(lines => Promise.resolve(lines.reduce((para, line) => para += `\t${line.text}\n\r${line.translation.text}\n\r`, '')));
 };
 
 const saveHukamnama = (hymn = randomAng, filename = defaultFilename()) => getHukamnama(hymn)
-  .then(hukamnama => {
-    return fs.writeFile(filename, hukamnama, 'utf8')
-  })
+  .then(hukamnama => fs.writeFile(filename, hukamnama, 'utf8'))
   .catch(e => console.log(`Error: ${e}, ${JSON.stringify(e, null, 2)}`));
 
 saveHukamnama(hymn, filename);
